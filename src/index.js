9aeebb3a8bcd1c9d7d25a29c36f071f83d2e727b
Nika-Bushinova <n190696@yandex.ru>
2022-06-23 17:07:46 -0400

    X

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-  let numberReverse= Math.abs(n).toString().split('').reverse().join('')
-         return +numberReverse}
+  let numberReverse2= Math.abs(n).toString().split('').reverse().join('')
+         return +numberReverse2}
